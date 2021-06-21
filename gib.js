26b4f4f9010cb80c56867045ef634e1ac6aca326
Alexander Mozeak <alex@astar.app>
2021-06-21 18:42:59 -0400

    Minor edits.

--- gib.js
+++ gib.js
@@ -6,14 +6,6 @@ button1.addEventListener("click", () => {
   console.log("Clicked the button!");
 })
 
-
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
-
 // chrome.runtime.onInstalled.addListener(() => {
 //   console.log("Yo!");
 //   if ('serviceWorker' in navigator) {
