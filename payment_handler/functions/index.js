36528a073735435b80898abf61acb573ed7c9206
Alexander Mozeak <alex@astar.app>
2021-07-11 23:01:38 -0400

    Refine extension implementation and service worker.

--- payment_handler/functions/index.js
+++ payment_handler/functions/index.js
@@ -1,9 +1,9 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.pay = functions.https.onRequest((req, res) => {
+    // Validate the Gib card.
+    // Match the card with a wallet.
+    // Begin streaming payment to Charlie's pointer.
+    console.log("Payment request ", req);
+    res.status(200).send(); // Respond to  payment request.
+})
