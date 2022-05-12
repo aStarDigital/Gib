88cd1d414de25f416bf901ecad6c28eef4fa1613
Tyler Altenhofen <tyleraltenhofen@gmail.com>
2022-05-12 18:53:26 -0500

    fixed extension functionality and added css framework

--- extension/app.js
+++ extension/app.js
@@ -1,11 +1,11 @@
 self.addEventListener('custompaymentrequest', async (e) => {
-    const requestOptions = {
-        headers: {
-            'Content Type': 'application/json'
-        },
-        body: JSON.stringify(e.detail.methodData)
-    };
+  const requestOptions = {
+    headers: {
+      'Content Type': 'application/json'
+    },
+    body: JSON.stringify(e.detail.methodData)
+  };
 
-    const response = await fetch("https://gib-app-1c49c.web.app/pay", requestOptions);
-    console.log(response);
+  const response = await fetch("https://gib-app-1c49c.web.app/pay", requestOptions);
+  console.log(response);
 });
