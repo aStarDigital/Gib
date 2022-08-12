201aa4e3508becdeb4e8fe83f0ed738bc5e49970
Alexander Mozeak <alex@astar.app>
2022-08-12 16:04:24 -0400

    Add terms and conditions.

--- backend/server.js
+++ backend/server.js
@@ -92,16 +92,6 @@ app.get('/feedback', (req, res, next) => {
   }
 })
 
-app.get('/terms', (req, res, next) => {
-  try {
-    ejs.renderFile("templates/terms.html", {}, {}, function (err, str) {
-      return res.send(str)
-    });
-  } catch (err) {
-    next(err)
-  }
-})
-
 app.get('/privacy', (req, res, next) => {
   try {
     ejs.renderFile("templates/privacy.html", {}, {}, function (err, str) {
